81154f6275ecdc1c4958478360e545d9f6af93f5
Lucas ROLLIN <lucas.rollin06@gmail.com>
2026-06-04 20:37:04 +0200

    feat(backend): add optionalAuthenticate middleware

---
+++ backend/src/middlewares/optionalAuthenticate.ts
@@ -0,0 +1,36 @@
+import { RequestHandler } from 'express';
+import { hashToken } from '../utils/sessionToken';
+import { findSessionByTokenHash, deleteSessionByTokenHash } from '../repositories/sessionRepository';
+
+export const optionalAuthenticate: RequestHandler = async (req, _res, next) => {
+  const rawToken = req.cookies?.session;
+  if (!rawToken) {
+    return next();
+  }
+
+  const tokenHash = hashToken(rawToken);
+  const session = await findSessionByTokenHash(tokenHash);
+  if (!session) {
+    return next();
+  }
+
+  if (session.expiresAt <= new Date()) {
+    deleteSessionByTokenHash(tokenHash).catch(() => {});
+    return next();
+  }
+
+  if (session.user.deletedAt !== null) {
+    deleteSessionByTokenHash(tokenHash).catch(() => {});
+    return next();
+  }
+
+  req.user = {
+    id: session.user.id,
+    email: session.user.email,
+    username: session.user.username,
+  };
+
+  req.sessionTokenHash = tokenHash;
+
+  next();
+};
