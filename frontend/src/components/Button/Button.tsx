f17ddf4ba02fed96fed2ccc6b87a5026b3751544
Lucas ROLLIN <lucas.rollin06@gmail.com>
2026-06-29 13:13:32 +0200

    feat(ui): add danger variant to Button

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -1,11 +1,13 @@
 import type { ComponentProps } from 'react';
 import styles from './Button.module.scss';
 
-type ButtonProps = ComponentProps<'button'>;
+type ButtonProps = ComponentProps<'button'> & {
+  variant?: 'primary' | 'danger';
+};
 
-const Button = ({ children, ...rest }: ButtonProps) => {
+const Button = ({ variant = 'primary', children, ...rest }: ButtonProps) => {
   return (
-    <button {...rest} className={styles.button}>
+    <button {...rest} className={`${styles.button} ${styles[variant]}`}>
       {children}
     </button>
   );
