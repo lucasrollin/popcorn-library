8e0f6959054192aa452b844e36b44897f2fbe67b
Lucas ROLLIN <lucas.rollin06@gmail.com>
2026-07-02 14:26:36 +0200

    feat(users): add PATCH /api/users/me route

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,8 +1,23 @@
 import { Router } from 'express';
-import { getPublicProfileController } from '../controllers/userController';
+import { getPublicProfileController, updateProfileController } from '../controllers/userController';
+import { z } from 'zod';
+import { validateBody } from '../middlewares/validate';
+import { authenticate } from '../middlewares/authenticate';
 
 const router = Router();
 
+const updateProfileSchema = z.object({
+  username: z
+    .string()
+    .trim()
+    .min(3)
+    .max(20)
+    .regex(/^[a-zA-Z0-9_]+$/)
+    .optional(),
+  avatar: z.string().url().nullable().optional(),
+});
+
+router.patch('/me', authenticate, validateBody(updateProfileSchema), updateProfileController);
 router.get('/:username', getPublicProfileController);
 
 export default router;
